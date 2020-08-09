2a5e50fabcb18239d6c7d568301b197306bfafe5
David Larpent <david.larpent@me.com>
2020-08-09 22:13:52 +0100

    Add content editing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
+import { GetStaticProps } from 'next'
 
-export default function Home() {
-  return (
+
+export default function Home({ file }) {
+const data = file.data
+
+return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -11,7 +16,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {data.title}
         </h1>
 
         <p className={styles.description}>
@@ -46,20 +51,38 @@ export default function Home() {
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
+          </a>s
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>Footer</p>
       </footer>
     </div>
   )
 }
+
+
+export const getStaticProps: GetStaticProps = async function({
+   preview,
+   previewData,
+  }) {
+   if (preview) {
+     return getGithubPreviewProps({
+       ...previewData,
+       fileRelativePath: 'content/home.json',
+       parse: parseJson,
+     })
+   }
+   return {
+     props: {
+       sourceProvider: null,
+       error: null,
+       preview: false,
+       file: {
+         fileRelativePath: 'content/home.json',
+         data: (await import('../content/home.json')).default,
+       },
+     },
+   }
+  }
