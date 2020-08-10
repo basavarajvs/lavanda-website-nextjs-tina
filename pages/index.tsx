c1a3e202cf513b0b200301f32e22b77bfa608c95
David Larpent <david.larpent@me.com>
2020-08-10 17:33:53 +0100

    Add inline editing

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,11 @@ import styles from "../styles/Home.module.css";
 import { getGithubPreviewProps, parseJson } from "next-tinacms-github";
 import { GetStaticProps } from "next";
 import { usePlugin } from "tinacms";
-import { useGithubJsonForm, useGithubToolbarPlugins } from "react-tinacms-github";
+import {
+  useGithubJsonForm,
+  useGithubToolbarPlugins,
+} from "react-tinacms-github";
+import { InlineForm, InlineTextField } from 'react-tinacms-inline'
 
 export default function Home({ file }) {
   const formOptions = {
@@ -15,7 +19,7 @@ export default function Home({ file }) {
   const [data, form] = useGithubJsonForm(file, formOptions);
   usePlugin(form);
 
-  useGithubToolbarPlugins()
+  useGithubToolbarPlugins();
 
   return (
     <div className={styles.container}>
@@ -23,44 +27,18 @@ export default function Home({ file }) {
         <title>{data.title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <InlineForm form={form}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            <InlineTextField name="title" />
+          </h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>{data.title}</h1>
-
-        <p className={styles.description}>
-          Get started by fandangoing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-          s
-        </div>
-      </main>
-
+          <p className={styles.description}>
+            Get started by fandangoing{" "}
+            <code className={styles.code}>pages/index.js</code>
+          </p>
+        </main>
+      </InlineForm>
       <footer className={styles.footer}>
         <p>Footer</p>
       </footer>
